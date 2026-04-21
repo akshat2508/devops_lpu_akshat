146dd643f93895615ad71bce16495b4d8a228812
Akshat-paul <akshatpaul2006@gmail.com>
2026-04-21 19:26:23 +0530

    Devops initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+#basic flask app 
+# containerize it 
+# we will automate the build (using git)
+# we will deploy it to cloud aws ec2 
+
+from flask import flask 
+
+app = flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1> devops successfull</h1> <p> pthon + docker + git + aws running .. created by akshat </p>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',  port = 5000)
